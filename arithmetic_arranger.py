43aed2be159f0574ff49a1dc00eb6f6c2f540e72
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-20 12:14:25 +0000

    top row working

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,5 @@
 def arithmetic_arranger(problems, answer=False):
+    # error handling 
     if len(problems) > 5: return "Error: Too many problems."
     for problem in problems:
         if "/" in problem or "*" in problem: return "Error: Operator must be '+' or '-'."
@@ -7,10 +8,51 @@ def arithmetic_arranger(problems, answer=False):
         if len(operand_one) > 4 or len(operand_two) > 4: 
             return "Error: Numbers cannot be more than four digits."
         try:
-            int_one = int(operand_one)
-            int_two = int(operand_two)
+            int(operand_one)
+            int(operand_two)
         except:
             return "Error: Numbers must only contain digits."
 
+    # figure out max length of operand in each problem
+    max_length_list = list()
+    for problem in problems:
+        length_list = []
+        operands = problem.split()
+        for operand in operands:
+            length_list.append(len(operand))
+        max_length_list.append(max(length_list))
+    
+    #build top row
+    top_row = "  "
+    index = 0
+    for problem in problems: 
+        operand_one = problem.split()[0]
+        operand_two = problem.split()[2]
+        operand_one_length = len(problem.split()[0])
+        operand_two_length = len(problem.split()[2])
+        len_difference = operand_one_length - operand_two_length
+        while len_difference < 0:
+            top_row = top_row + " "
+            len_difference = len_difference + 1
+        top_row = top_row + operand_one
+        if index < len(problems) - 1:
+            top_row = top_row + "      "
+        else:
+            top_row = top_row + "\n"
+        index = index + 1
+    #build bottom row
+    bottom_row = ""
+    index = 0
+    for problem in problems:
+        operand_one = problem.split()[0]
+        operator = problem.split()[1]
+        operand_two = problem.split()[2]
+        operand_one_length = len(problem.split()[0])
+        operand_two_length = len(problem.split()[2])
+        bottom_row = bottom_row + operator
+        len_difference = operand_two_length - operand_one_length
+        while len_difference < 0:
+            bottom_row = bottom_row + " "
+            len_difference + 1
         
-    return answer
+    return 1
