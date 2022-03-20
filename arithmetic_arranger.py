bac1741d91484bdabb90612f17d8d342a01c504e
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-20 11:29:17 +0000

    4 tests passing

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,4 +2,15 @@ def arithmetic_arranger(problems, answer=False):
     if len(problems) > 5: return "Error: Too many problems."
     for problem in problems:
         if "/" in problem or "*" in problem: return "Error: Operator must be '+' or '-'."
+        operand_one = problem.split()[0]
+        operand_two = problem.split()[2]
+        if len(operand_one) > 4 or len(operand_two) > 4: 
+            return "Error: Numbers cannot be more than four digits."
+        try:
+            int_one = int(operand_one)
+            int_two = int(operand_two)
+        except:
+            return "Error: Numbers must only contain digits."
+
+        
     return answer
